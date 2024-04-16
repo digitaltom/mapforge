c28fc6cbe867d62ce1518b4cb4b3b157a9c96cf4
Thomas Schmidt <tschmidt@suse.de>
2024-04-17 00:51:35 +0200

    figure out icon layer in deck.gl

--- app/javascript/deck.js
+++ app/javascript/deck.js
@@ -14,54 +14,82 @@ const deck = window.deck;
 async function init () {
   const map = new maplibregl.Map({
     container: 'deck-map', // container ID
-    style: 'https://basemaps.cartocdn.com/gl/positron-gl-style/style.json',
+    // style: 'https://basemaps.cartocdn.com/gl/positron-gl-style/style.json',
+    style: '/layers/nostreets.json?key=' + window.gon.map_keys.maptiler,
     center: [8.271366455078127, 50.013330503465454],
-    zoom: 9
+    zoom: 9,
+    pitch: 45 // tilt the map
   })
-  await map.once('load')
+  map.addControl(new maplibregl.NavigationControl())
+
+  // await map.once('load')
+
+  const data = [
+    { position: [8.271366455078127, 50.013330503465454], iconUrl: '/icons/mapforge-logo.png', size: 100 },
+    { position: [8.371366, 50.11333], iconUrl: '/icons/mapforge-logo.png', size: 150 }
+    // Add more points as needed
+  ]
+
+  const iconLayer = new deck.IconLayer({
+    id: 'icon-layer',
+    data,
+    pickable: true,
+    // Auto-pack icons by providing icon URLs directly
+    getIcon: d => ({
+      url: d.iconUrl,
+      height: 100,
+      width: 100,
+      anchorY: 128
+    }),
+    onIconError: e => console.log(e),
+    getSize: 40
+    // getPosition: d => d.position,
+  })
+
+  // https://deck.gl/docs/api-reference/layers/geojson-layer
+  const geojsonLayer = new deck.GeoJsonLayer({
+    id: 'airports',
+    data: '/maps/frontpage-category-friends/features',
+    // Styles
+    filled: true,
+    // stroked: false,
+    pointType: 'icon', // 'icon+circle+text',
+
+    getIcon: d => ({
+      url: d.properties['marker-icon'],
+      height: 100,
+      width: 100,
+      anchorY: 128
+    }),
+    onIconError: e => console.log(e),
+    getIconSize: 40,
+    // sizeScale: 10,
+    // sizeMinPixels: 20,
+    // pickable: true,
+    // getPosition: d => d.position,
+
+    pointRadiusMinPixels: 2,
+    pointRadiusScale: 10,
+    getPointRadius: f => 128,
+    getFillColor: [200, 0, 80, 180],
+    // // Interactive props
+    // pickable: true,
+    // autoHighlight: true,
+    getText: f => f.properties['marker-icon'],
+    getTextSize: 12
+    // onClick: info =>
+    //   // eslint-disable-next-line
+    //   info.object && alert(`${info.object.properties['marker-icon']} (${info.object.properties.abbrev})`)
+    // // beforeId: 'watername_ocean' // In interleaved mode, render the layer under map labels
+  })
 
   const deckOverlay = new deck.MapboxOverlay({
     // interleaved: true,
     layers: [
-      new deck.GeoJsonLayer({
-        id: 'airports',
-        data: '/maps/frontpage-category-friends/features',
-        // Styles
-        filled: true,
-        pointRadiusMinPixels: 2,
-        pointRadiusScale: 2000,
-        getPointRadius: f => 11 - f.properties.scalerank,
-        getFillColor: [200, 0, 80, 180],
-        // Interactive props
-        pickable: true,
-        autoHighlight: true,
-        onClick: info =>
-          // eslint-disable-next-line
-          info.object && alert(`${info.object.properties.name} (${info.object.properties.abbrev})`)
-        // beforeId: 'watername_ocean' // In interleaved mode, render the layer under map labels
-      }),
-      new deck.IconLayer({
-        id: 'icon-layer',
-        data: '/maps/frontpage-category-friends/features', // URL to your GeoJSON data
-        // Define how to extract the icon URL and position from each feature
-        getIcon: d => ({
-          url: d.properties.markerIcon,
-          width: 128,
-          height: 128,
-          anchorY: 128 // Adjust based on your image's anchor point
-        }),
-        sizeScale: 1, // Adjust the scale as needed
-        getPosition: d => d.geometry.coordinates,
-        getSize: d => 1, // Adjust size as needed, or use a function to determine size dynamically
-        pickable: true,
-        // Optional: Define onClick behavior
-        onClick: ({ object, x, y }) => {
-          console.log('Clicked on', object)
-        }
-      })
+      geojsonLayer,
+      iconLayer
     ]
   })
 
   map.addControl(deckOverlay)
-  map.addControl(new maplibregl.NavigationControl())
 }
