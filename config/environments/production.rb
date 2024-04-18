92d5562ab3635f33cbdaae085dbe209962fc2ec9
Thomas Schmidt <tschmidt@suse.de>
2024-04-18 23:44:35 +0200

    allow http

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,8 @@ Rails.application.configure do
   # config.assume_ssl = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  # Allow healthcheck per http. http -> https redirection is done by Traefik.
+  config.force_ssl = false
 
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new($stdout)
