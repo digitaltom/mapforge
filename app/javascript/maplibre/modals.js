ae318d794d62b2da642fc240e7ad50c91fef846a
Thomas Schmidt <tschmidt@suse.de>
2024-06-15 00:35:10 +0200

    feature details modal

--- app/javascript/maplibre/modals.js
+++ app/javascript/maplibre/modals.js
@@ -1,20 +1,36 @@
 // eslint expects variables to get imported, but we load the full lib in header
 const turf = window.turf
 
-export function showFeatureDetails (feature) {
-  document.querySelector('#feature-details-modal').style.display = 'block'
-  let desc = feature?.properties?.desc
+function featureTitle (feature) {
   const title = feature?.properties?.title || feature?.properties?.label
+  if (!title || title === '') {
+    return '<i>No title</i>'
+  }
+  return title
+}
+
+function featureMeta (feature) {
+  let meta = feature.id
+  if (feature.geometry.type === 'Point') {
+    meta += '<br>' + feature.geometry.coordinates
+  }
   if (feature.geometry.type === 'LineString') {
     const turfLineString = turf.lineString(feature.geometry.coordinates)
     const length = turf.length(turfLineString)
-    desc += '<br>Length: ' + Math.round(length * 1000) + 'm'
+    meta += '<br>' + Math.round(length * 1000) + 'm'
   }
   if (feature.geometry.type === 'Polygon') {
     const turfPolygon = turf.polygon(feature.geometry.coordinates)
     const area = turf.area(turfPolygon)
-    desc += '<br>Area: ' + (area / 1000000).toFixed(2) + ' km²'
+    meta += '<br>' + (area / 1000000).toFixed(2) + ' km²'
   }
-  document.querySelector('#feature-details-header').innerHTML = title
-  document.querySelector('#feature-details-body').innerHTML = desc
+  return meta
+}
+
+export function showFeatureDetails (feature) {
+  document.querySelector('#feature-details-modal').style.display = 'block'
+  const desc = feature?.properties?.desc
+  document.querySelector('#feature-details-header').innerHTML = featureTitle(feature)
+  document.querySelector('#feature-details-meta').innerHTML = featureMeta(feature)
+  if (desc) { document.querySelector('#feature-details-body').innerHTML = desc }
 }
