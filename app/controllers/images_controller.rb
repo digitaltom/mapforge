6b9cc6efa69b1e16c46a55849660285240a869ee
Thomas Schmidt <tschmidt@suse.de>
2024-07-07 22:37:03 +0200

    allow link to full image

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,10 +1,16 @@
 class ImagesController < ApplicationController
-  before_action :set_image, only: %i[icon]
+  before_action :set_image, only: %i[icon image]
 
   def icon
     expires_in 60.minutes, public: true
     # resize, crop if necessary to maintain aspect ratio (centre gravity)
     image_url = @image.img.thumb("100x100#", quality: 95).rounded.url
+    redirect_to image_url
+  end
+
+  def image
+    expires_in 60.minutes, public: true
+    image_url = @image.img.url
     redirect_to image_url
   end
 
