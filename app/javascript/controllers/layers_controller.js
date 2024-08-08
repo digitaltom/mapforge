26692a9a536281cc6f214731f15b9db086298b06
Thomas Schmidt <tschmidt@suse.de>
2024-08-09 00:15:09 +0200

    highlight feature after select from layer

--- app/javascript/controllers/layers_controller.js
+++ app/javascript/controllers/layers_controller.js
@@ -75,7 +75,7 @@ export default class extends Controller {
     const centroid = turf.centroid(feature)
     console.log('Fly to: ' + feature.id + ' ' + centroid.geometry.coordinates)
     resetControls()
-    map.once('moveend', function () { highlightFeature(feature) })
+    map.once('moveend', function () { highlightFeature(feature, true) })
     map.flyTo({
       center: centroid.geometry.coordinates,
       duration: 1000,
