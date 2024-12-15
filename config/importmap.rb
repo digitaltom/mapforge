5460678c74a289a6f8986c7fee5706edbe4451b5
Thomas Schmidt <tschmidt@suse.de>
2024-12-15 01:21:00 +0100

    update mapbox draw

--- config/importmap.rb
+++ config/importmap.rb
@@ -21,9 +21,7 @@ pin "animate"
 
 # vendor
 # examples: https://generator.jspm.io/
-# https://github.com/mapbox/mapbox-gl-draw
-pin "@mapbox/mapbox-gl-draw", to: "https://ga.jspm.io/npm:@mapbox/mapbox-gl-draw@1.4.3/dist/mapbox-gl-draw.js",
-preload: false
+
 # https://github.com/zakjan/mapbox-gl-draw-waypoint
 pin "mapbox-gl-draw-waypoint",
   to: "https://ga.jspm.io/npm:mapbox-gl-draw-waypoint@1.2.3/dist/mapbox-gl-draw-waypoint.esm.js", preload: false
@@ -40,3 +38,12 @@ pin "maplibre-gl", to: "https://ga.jspm.io/npm:maplibre-gl@4.7.1/dist/maplibre-g
 pin "maptiler-geocoding-control", to: "https://ga.jspm.io/npm:@maptiler/geocoding-control@1.4.1/maplibregl.js"
 # render markdown
 pin "marked", to: "https://ga.jspm.io/npm:marked@15.0.3/lib/marked.esm.js"
+
+# https://github.com/mapbox/mapbox-gl-draw
+pin "@mapbox/mapbox-gl-draw", to: "@mapbox--mapbox-gl-draw.js", preload: false # @1.5.0
+pin "@mapbox/geojson-area", to: "@mapbox--geojson-area.js", preload: false # @0.2.2
+pin "@mapbox/geojson-normalize", to: "@mapbox--geojson-normalize.js", preload: false # @0.0.1
+pin "@mapbox/point-geometry", to: "@mapbox--point-geometry.js", preload: false # @1.1.0
+pin "fast-deep-equal", preload: false # @3.1.3
+pin "nanoid/non-secure", to: "nanoid--non-secure.js", preload: false # @5.0.9
+pin "wgs84", preload: false # @0.0.0
