ff41b27fd6f4c3041a14d32b52b3aca34d60a7d1
Thomas Schmidt <tschmidt@suse.de>
2024-12-28 23:21:24 +0100

    update mongoid configs

--- config/initializers/mongoid.rb
+++ config/initializers/mongoid.rb
@@ -1,18 +1,24 @@
-require "mongo"
+Mongoid.configure do
+  target_version = "9.0"
 
-begin
-  Mongoid.load!(File.join(Rails.root, "config", "mongoid.yml"))
+  # Load Mongoid behavior defaults. This automatically sets
+  # features flags (refer to documentation)
+  config.load_defaults target_version
 
-  # skip test in asset precompilation
-  return if ENV["SECRET_KEY_BASE_DUMMY"]
-
-  Mongoid.client(:default).command({ isMaster: 1 })
-
-  # mongoid config
-  Mongoid.raise_not_found_error = false
-  Mongoid::Config.immutable_ids = true
-
-rescue Mongo::Error::NoServerAvailable => e
-  puts "Could not connect to MongoDB. #{e.message}"
-  exit 1
+  # It is recommended to use config/mongoid.yml for most Mongoid-related
+  # configuration, whenever possible, but if you prefer, you can set
+  # configuration values here, instead:
+  #
+  #   config.log_level = :debug
+  #
+  # Note that the settings in config/mongoid.yml always take precedence,
+  # whatever else is set here.
 end
+
+# Enable Mongo driver query cache for Rack
+# Rails.application.config.middleware.use(Mongo::QueryCache::Middleware)
+
+# Enable Mongo driver query cache for ActiveJob
+# ActiveSupport.on_load(:active_job) do
+#   include Mongo::QueryCache::Middleware::ActiveJob
+# end
