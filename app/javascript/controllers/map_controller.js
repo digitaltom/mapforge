77543a044cdf1cebed9be477d46dbc53297f8d93
Thomas Schmidt <tschmidt@suse.de>
2025-01-23 00:08:17 +0100

    offset modals on navbar show

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -10,6 +10,7 @@ export default class extends Controller {
   toggleNavigation (event) {
     if (document.querySelector('#map-header').style.display === 'none') {
       functions.e('#map-header', e => { e.style.display = 'block' })
+      functions.e('.map-modal', e => { e.style.top = '4em' })
       functions.e('#map-nav-toggle', e => {
         e.classList.remove('bi-caret-down')
         e.classList.add('bi-caret-up')
@@ -20,6 +21,7 @@ export default class extends Controller {
       animateElement('.navbar', 'fade-down')
     } else {
       functions.e('#map-header', e => { e.style.display = 'none' })
+      functions.e('.map-modal', e => { e.style.top = '0' })
       functions.e('#map-nav-toggle', e => {
         e.classList.remove('bi-caret-up')
         e.classList.add('bi-caret-down')
